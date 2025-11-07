3f9cbb3232811c819ae01e683083a1c49c67820c
HemaSasankSeelam <seelamhemasasank@gmail.com>
2025-11-07 16:00:00 +0530

    updated allowed host

--- selenium_app/settings.py
+++ selenium_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-foz#cp4q$t@s9p!kl8--)c47fwly8$28#lt%cqj^-6@c(!$-@^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1","render-digilocker.onrender.com"]
 
 
 # Application definition
